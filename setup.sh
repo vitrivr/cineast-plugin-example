a98169313d60c5b9ad381150d682ef500ab9e79f
Luca Rossetto <luca.rossetto@unibas.ch>
2017-10-09 17:32:46 +0200

    Added cineast deploy command to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 #This script initializes the code dependencies to Cineast and starts an initial gradle build to initialize gradle dependencies as well.
 
 git clone https://github.com/vitrivr/cineast.git cineast
+./gradlew cineast:deploy
 ./gradlew build
